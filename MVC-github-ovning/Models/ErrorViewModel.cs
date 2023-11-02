55031c530d8da1c5ba45715c5ea477c1928f8216
EddMal <107246914+EddMal@users.noreply.github.com>
2023-11-02 09:50:24 +0100

    Add project files.

---
+++ MVC-github-ovning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_github_ovning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
